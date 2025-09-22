3ae1dcf553cb138d3c9ead654507ee6ac3b15bf5
Raz Ahamed <raz.abcoder@gmail.com>
2025-09-22 11:20:45 +0600

    slice ready

--- src/features/counters/countersSlice.js
+++ src/features/counters/countersSlice.js
@@ -1,11 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
-const initialState = {
-    value: 0,
-};      
+
+const initialState = [
+  { id: 1, value: 0 },
+  { id: 2, value: 0 },
+]
 export const counterSlice = createSlice({
     name: 'counters',
     initialState,
     reducers: {         
+        // action.payload
         increment:  (state, action) => {
             state.value += 1;
         },
